6bfb86ec3cbb59153085fa3264fc483c449e6013
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 11:18:54 -0300

    Update 18.c

--- Lista 1/18.c
+++ Lista 1/18.c
@@ -1,10 +1,14 @@
-// 18 ≠FaÁa um programa que calcule e mostre a ·rea de um tri‚ngulo. Supondo que: ¡rea = (base *altura) / 2.
+// 18 ¬≠Fa√ßa um programa que calcule e mostre a √°rea de um tri√¢ngulo. Supondo que: √Årea = (base *altura) / 2.
 
 #include <stdio.h>
 
+float t (float b, float a){
+	return ((b*a)/2);
+}
+
 main(){
-	int b, a;
-	scanf("%d %d",&b,&a);
-	printf ("%d\n",(b*a)/2);
+	float b, a;
+	scanf("%f %f",&b,&a);
+	printf ("%f\n",t(b, a));
 	return 0;
 }
