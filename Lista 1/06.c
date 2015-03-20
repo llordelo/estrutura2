5492eaa5e7b8adff4f61c33b89a38ee6ab24fbde
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 10:56:00 -0300

    Update 06.c

--- Lista 1/06.c
+++ Lista 1/06.c
@@ -1,10 +1,10 @@
-// 6­ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+// 4­ Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
 
 #include <stdio.h>
 
 float media (float n1, float n2, float n3, float n4){
 	
-	return ((n1+n2+n3+n4)/4);
+	return (n1+n2+n3+n4);
 }
 
 main(){
