15f5c5bad59baefbd707b9137df589f02fc8c173
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 10:54:16 -0300

    Update 05.c

--- Lista 1/05.c
+++ Lista 1/05.c
@@ -1,11 +1,15 @@
-// 5­ Faça um Programa que peça dois números e imprima a soma.
+// 5Â­ FaÃ§a um Programa que peÃ§a dois nÃºmeros e imprima a soma.
 
 #include <stdio.h>
+
+float soma (float x, float y){
+	return x+y;
+}
 
 main(){
 	float x1, x2;
 
 	scanf ("%f %f",&x1, &x2);
-	printf ("%.2f\n",x1 + x2);
+	printf ("%f\n",soma(x1, x2));
 	return 0;
 }
