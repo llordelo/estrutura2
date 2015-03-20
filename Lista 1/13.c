4ffc0a6f82b36508595a201061e2603196a69ed8
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 11:13:22 -0300

    Update 13.c

--- Lista 1/13.c
+++ Lista 1/13.c
@@ -1,10 +1,14 @@
-// 13­ Faça um programa que receba três notas e seus respectivos pesos, calcule e mostre a média ponderada.
+// 13Â­ FaÃ§a um programa que receba trÃªs notas e seus respectivos pesos, calcule e mostre a mÃ©dia ponderada.
 
 #include <stdio.h>
+
+float notas (float n1, float p1, float n2, float p2, float n3, float p3){
+	return ((n1*p1)+(n2*p2)+(n3*p3)/(p1+p2+p3));
+}
 
 main(){
 	float n1, p1, n2, p2, n3, p3;
 	scanf("%d %d %d",&n1,&p1,&n2,&p2,&n3,&p3);
-	printf ("%.2f\n",((n1*p1)+(n2*p2)+(n3*p3)/(p1+p2+p3);
+	printf ("%.2f\n",notas(n1, p1, n2, p2, n3, p3));
 	return 0;
 }
