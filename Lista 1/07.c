133e8335a4a9ddee5f6e054ccb0cce685762663d
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 10:51:12 -0300

    Update 07.c

--- Lista 1/07.c
+++ Lista 1/07.c
@@ -1,14 +1,16 @@
-// 7­ Faça um Programa que converta metros para centímetros.
+// 6­ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 #include <stdio.h>
 
-int con (int x){
-	return x*100;
+float media (float n1, float n2, float n3, float n4){
+	
+	return ((n1+n2+n3+n4)/4);
 }
 
 main(){
-	int x;
-	scanf("%d",&x);
-	printf ("%d\n",con(x));
+	float n1, n2, n3, n4;
+	
+	scanf ("%f %f %f %f",&n1,&n2,&n3,&n4);
+	printf ("%f\n",media(n1,n2,n3,n4));
 	return 0;
 }
