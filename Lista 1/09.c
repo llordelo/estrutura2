ad6617cf92b79275ff562b6d5d8f13b3b542c259
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 10:39:35 -0300

    Update 09.c

--- Lista 1/09.c
+++ Lista 1/09.c
@@ -1,11 +1,14 @@
-// 9­ Faça um Programa que leia o tamanho de um lado do quadrado e calcule sua área em seguida mostre o dobro
+// 9Â­ FaÃ§a um Programa que leia o tamanho de um lado do quadrado e calcule sua Ã¡rea em seguida mostre o dobro
 
 #include <stdio.h>
 
+int area (int l){
+	return l*l;
+}
 main(){
 	int l;
 	scanf("%d",&l);
-	printf ("%d\n",l*l);
-	printf ("%d\n",(l*l)*2);
+	printf ("%d\n",area(l));
+	printf ("%d\n",area(l)*2);
 	return 0;
 }
