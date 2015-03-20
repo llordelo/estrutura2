41190fba2f7898ad75bcd162e88d2aab0ca4e767
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 10:42:01 -0300

    Update 08.c

--- Lista 1/08.c
+++ Lista 1/08.c
@@ -1,10 +1,14 @@
-// 8­ Ler o valor do raio de um circulo e calcular a área do circulo correspondente, considere pi = 3:141592.
+// 8Â­ Ler o valor do raio de um circulo e calcular a Ã¡rea do circulo correspondente, considere pi = 3:141592.
 
 #include <stdio.h>
+
+float area (float r){
+	return 3.141592 * r*r
+}
 
 main(){
 	int r;
 	scanf("%d",&r);
-	printf ("%f\n",3.141592 * r*r);
+	printf ("%f\n",area(r));
 	return 0;
 }
