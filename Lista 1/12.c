7a315681b5b59da3641f5fa3cc03437840157dc4
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 11:15:18 -0300

    Update 12.c

--- Lista 1/12.c
+++ Lista 1/12.c
@@ -1,10 +1,14 @@
-// 12 ­ Faça um programa que receba três notas, calcule e mostre a média aritmética entre elas.
+// 12 Â­ FaÃ§a um programa que receba trÃªs notas, calcule e mostre a mÃ©dia aritmÃ©tica entre elas.
 
 #include <stdio.h>
 
+float med (float n1, float n2, float n3){
+	return ((n1+n2+n3)/3);
+}
+
 main(){
-	int n1, n2, n3;
-	scanf("%d %d %d",&n1,&n2,&n3);
-	printf ("%.2f\n",(n1+n2+n3)/3);
+	float n1, n2, n3;
+	scanf("%f %f %f",&n1,&n2,&n3);
+	printf ("%.2f\n",med(n1, n2, n3));
 	return 0;
 }
