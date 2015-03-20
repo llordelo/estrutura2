9f37941a07186825497dc4f7f34e1c824f52937c
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 11:17:09 -0300

    Update 11.c

--- Lista 1/11.c
+++ Lista 1/11.c
@@ -1,10 +1,14 @@
-// 11­ Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius. C=(5*(F32)/9).
+// 11Â­ FaÃ§a um Programa que peÃ§a a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius. C=(5*(F32)/9).
 
 #include <stdio.h>
 
+float t (f){
+	return (5*(f-32)/9);
+}
+
 main(){
-	int f;
-	scanf("%d",&f);
-	printf ("%.2f\n",5*(f-32)/9);
+	float f;
+	scanf("%f",&f);
+	printf ("%.2f\n",t(f));
 	return 0;
 }
