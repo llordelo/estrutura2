8cf2a3c255e28f3029ce7ac03556c2549b527809
Danilo Brito <danilo.britoxd@hotmail.com>
2015-03-20 11:09:44 -0300

    Update 14.c

--- Lista 1/14.c
+++ Lista 1/14.c
@@ -1,10 +1,14 @@
-// 14­ Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo­se que este sofreu um aumento de 25%.
+// 14Â­ FaÃ§a um programa que receba o salÃ¡rio de um funcionÃ¡rio, calcule e mostre o novo salÃ¡rio, sabendoÂ­se que este sofreu um aumento de 25%.
 
 #include <stdio.h>
+
+float sa (float s){
+	return (s + ((s*25)/100));
+}
 
 main(){
 	float s;
 	scanf("%f",&s);
-	printf ("%.2f\n",s + ((s*25)/100);
+	printf ("%.2f\n",sa(s));
 	return 0;
 }
