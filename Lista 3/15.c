8e557388ebcd7f348813de6fac0f844b2eef99e6
Danilo Brito <danilo.britoxd@hotmail.com>
2015-04-07 00:17:43 -0300

    Create 15.c

---
+++ Lista 3/15.c
@@ -0,0 +1,19 @@
+15)
+
+a) x = 10
+b) *py= 5.9
+c) px = FFA0
+d) &y = FFB4
+e) *px = 10
+f) y = 5.9
+g) *ppx = 10
+h) py = FFB4
+i) &x = FFA0
+j) py++ = lixo
+k) *px-- = 9
+l) **ppy = 5.9
+m) &ppy = FFA6
+n) *&px = FFA0
+o) **ppx++ = lixo
+p) px++ = lixo
+q) &ppx = FFD4
